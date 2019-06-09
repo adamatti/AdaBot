13e417571bfec99a377dcbde840033cb933c437b
Marcelo Adamatti <adamatti@gmail.com>
2019-06-08 23:56:54 -0300

    Added health check

---
+++ src/main/groovy/adabot/bot/HealthController.groovy
@@ -0,0 +1,35 @@
+package adabot.bot
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.discovery.event.ServiceStartedEvent
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+import io.micronaut.scheduling.annotation.Async
+
+import java.lang.management.OperatingSystemMXBean
+
+@Slf4j
+@CompileStatic
+@Controller("/health")
+class HealthController implements ApplicationEventListener<ServiceStartedEvent> {
+    private Date startDate
+
+    @Get("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    Map getHealth(){
+        [
+            status: "ok",
+            startDate: startDate.toLocaleString()
+        ]
+    }
+
+    @Async
+    @Override
+    void onApplicationEvent(ServiceStartedEvent event) {
+        startDate = new Date()
+    }
+}
